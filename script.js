354f883bf2985174f7a2b1e065e2714a02184d4f
MhkMafia <mhk1mafia123@gmail.com>
2024-10-05 11:38:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+/* Sidebar Functionality */
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+}
